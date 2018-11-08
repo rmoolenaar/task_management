4dda341fae8d5fa97bec91adad6e9108f57632ba
Remco Moolenaar <remco@saskiaenremco.nl>
2018-11-08 17:08:13 +0100

    Fix security issues

--- app.js
+++ app.js
@@ -50,11 +50,6 @@ passport.deserializeUser(function(id, done) {
     done(null, id);
 });
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
